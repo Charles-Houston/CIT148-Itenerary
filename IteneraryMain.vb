8090f32e8035bf5e6cc669b2e51f8a7b0e258cda
Charles Houston <choustonagt@gmail.com>
2014-07-18 00:17:25 -0400

    Main declarations
    This is the Dim list for what we should be using throughout the program.
    Some will be specific to sections, but these are ones declared early and
    used throughout.

---
+++ IteneraryMain.vb
@@ -0,0 +1,20 @@
+Public Class frmItenerary
+    Dim locale As String
+    Dim flight As String
+    Dim depDate As Date
+    Dim depTime As Date
+    Dim depArrivalTime As Date
+    Dim retDate As Date
+    Dim retDepTime As Date
+    Dim retArrivalTime As Date
+    Dim flightCost As Double
+    Dim hotel As String
+    Dim hotelIn As Date
+    Dim hotelOut As Date
+    Dim hotelCostDaily As Double
+    Dim carRental As String
+    Dim carCostDaily As Double
+    Dim excursion()
+    Dim excursionCost()
+End Class
+	
